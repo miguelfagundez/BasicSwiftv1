df45bccaef16bd839998c473fe3cff9cc2226182
Miguel Fagundez <miguel.fagundez@capco.com>
2021-11-12 10:24:15 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicSwift
+//
+//  Created by MFCR on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
