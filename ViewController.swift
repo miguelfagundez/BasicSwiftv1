57c0e0b0cf70e4f5c1270111242038cfe567fbb8
Miguel Fagundez <miguel.fagundez@capco.com>
2021-11-22 09:37:14 -0500

    Connecting View
    ViewController connected with View

--- ViewController.swift
+++ ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // TextFields
+    @IBOutlet weak var valueTextField: UITextField!
+    @IBOutlet weak var valueCopyTextField: UITextField!
+    
+    // Labels
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Buttons
+    @IBAction func copyBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearBtn(_ sender: Any) {
+    }
+    
 }
 
