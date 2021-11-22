8c0e9208d328a01f758a6c62cc7007edf6d72916
Miguel Fagundez <miguel.fagundez@capco.com>
2021-11-22 12:26:38 -0500

    Some testing Logic

--- ViewController.swift
+++ ViewController.swift
@@ -10,23 +10,28 @@ import UIKit
 class ViewController: UIViewController {
 
     // TextFields
-    @IBOutlet weak var valueTextField: UITextField!
-    @IBOutlet weak var valueCopyTextField: UITextField!
+    @IBOutlet var oneTextField: UITextField!
+    @IBOutlet var twoTextField: UITextField!
     
     // Labels
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var resultLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = String("0")
         // Do any additional setup after loading the view.
     }
 
     // Buttons
     @IBAction func copyBtn(_ sender: UIButton) {
+        twoTextField.text = oneTextField.text! + String("_copy")
+        resultLabel.text = "\(Int(resultLabel.text!)! + 1)"
     }
     
     @IBAction func clearBtn(_ sender: Any) {
+        oneTextField.text = ""
+        twoTextField.text = ""
     }
     
 }
